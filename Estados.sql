b387f22472e475e637ecf317f1a97290172aa97c
Ronie <roniemeque@outlook.com>
2017-05-30 14:53:34 -0300

    Padronizações e relacionamentos.
    .vscode incluida para facilitar uso de utf-8 nos arquivos

--- Estados.sql
+++ Estados.sql
@@ -1,40 +1,46 @@
 -- Create Table --
 
-CREATE TABLE Estado (
-    Id       INT          NOT NULL AUTO_INCREMENT,
-    CodigoUf INT          NOT NULL,
-    Nome     VARCHAR (50) NOT NULL,
-    Uf       CHAR 	 (2)  NOT NULL,
-    Regiao   INT	      NOT NULL,
-    PRIMARY KEY (Id)
+CREATE TABLE estados (
+    id       INT          NOT NULL AUTO_INCREMENT,
+    coduf 	 INT          NOT NULL,
+    nome     VARCHAR (50) NOT NULL,
+    uf       CHAR 	 (2)  NOT NULL,
+    regiao   INT	      NOT NULL,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE estados
+ ADD PRIMARY KEY (id), ADD KEY uf (uf);
+
+ALTER TABLE estados
+MODIFY id int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=28;
 
 -- Insert Data --
 
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (12, 'Acre', 'AC', 1);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (27, 'Alagoas', 'AL', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (16, 'Amapá', 'AP', 1);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (13, 'Amazonas', 'AM', 1);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (29, 'Bahia', 'BA', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (23, 'Ceará', 'CE', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (53, 'Distrito Federal', 'DF', 5);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (32, 'Espírito Santo', 'ES', 3);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (52, 'Goiás', 'GO', 5);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (21, 'Maranhão', 'MA', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (51, 'Mato Grosso', 'MT', 5);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (50, 'Mato Grosso do Sul', 'MS', 5);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (31, 'Minas Gerais', 'MG', 3);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (15, 'Pará', 'PA', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (25, 'Paraíba', 'PB', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (41, 'Paraná', 'PR', 4);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (26, 'Pernambuco', 'PE', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (22, 'Piauí', 'PI', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (33, 'Rio de Janeiro', 'RJ', 3);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (24, 'Rio Grande do Norte', 'RN', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (43, 'Rio Grande do Sul', 'RS', 4);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (11, 'Rondônia', 'RO', 1);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (14, 'Roraima', 'RR', 1);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (42, 'Santa Catarina', 'SC', 4);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (35, 'São Paulo', 'SP', 3);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (28, 'Sergipe', 'SE', 2);
-Insert into Estado (CodigoUf, Nome, Uf, Regiao) values (17, 'Tocantins', 'TO', 1);
+Insert into estados (coduf, nome, uf, regiao) values (12, 'Acre', 'AC', 1);
+Insert into estados (coduf, nome, uf, regiao) values (27, 'Alagoas', 'AL', 2);
+Insert into estados (coduf, nome, uf, regiao) values (16, 'Amapá', 'AP', 1);
+Insert into estados (coduf, nome, uf, regiao) values (13, 'Amazonas', 'AM', 1);
+Insert into estados (coduf, nome, uf, regiao) values (29, 'Bahia', 'BA', 2);
+Insert into estados (coduf, nome, uf, regiao) values (23, 'Ceará', 'CE', 2);
+Insert into estados (coduf, nome, uf, regiao) values (53, 'Distrito Federal', 'DF', 5);
+Insert into estados (coduf, nome, uf, regiao) values (32, 'Espírito Santo', 'ES', 3);
+Insert into estados (coduf, nome, uf, regiao) values (52, 'Goiás', 'GO', 5);
+Insert into estados (coduf, nome, uf, regiao) values (21, 'Maranhão', 'MA', 2);
+Insert into estados (coduf, nome, uf, regiao) values (51, 'Mato Grosso', 'MT', 5);
+Insert into estados (coduf, nome, uf, regiao) values (50, 'Mato Grosso do Sul', 'MS', 5);
+Insert into estados (coduf, nome, uf, regiao) values (31, 'Minas Gerais', 'MG', 3);
+Insert into estados (coduf, nome, uf, regiao) values (15, 'Pará', 'PA', 2);
+Insert into estados (coduf, nome, uf, regiao) values (25, 'Paraíba', 'PB', 2);
+Insert into estados (coduf, nome, uf, regiao) values (41, 'Paraná', 'PR', 4);
+Insert into estados (coduf, nome, uf, regiao) values (26, 'Pernambuco', 'PE', 2);
+Insert into estados (coduf, nome, uf, regiao) values (22, 'Piauí', 'PI', 2);
+Insert into estados (coduf, nome, uf, regiao) values (33, 'Rio de Janeiro', 'RJ', 3);
+Insert into estados (coduf, nome, uf, regiao) values (24, 'Rio Grande do Norte', 'RN', 2);
+Insert into estados (coduf, nome, uf, regiao) values (43, 'Rio Grande do Sul', 'RS', 4);
+Insert into estados (coduf, nome, uf, regiao) values (11, 'Rondônia', 'RO', 1);
+Insert into estados (coduf, nome, uf, regiao) values (14, 'Roraima', 'RR', 1);
+Insert into estados (coduf, nome, uf, regiao) values (42, 'Santa Catarina', 'SC', 4);
+Insert into estados (coduf, nome, uf, regiao) values (35, 'São Paulo', 'SP', 3);
+Insert into estados (coduf, nome, uf, regiao) values (28, 'Sergipe', 'SE', 2);
+Insert into estados (coduf, nome, uf, regiao) values (17, 'Tocantins', 'TO', 1);
--- Regioes.sql
+++ Regioes.sql
@@ -1,15 +1,15 @@
 -- Create Table --
 
-CREATE TABLE Regiao (
-  Id 	INT 		NOT NULL AUTO_INCREMENT,
-  Nome 	VARCHAR(50) NOT NULL,
-  PRIMARY KEY (Id)
+CREATE TABLE regioes (
+  id 	INT 		NOT NULL AUTO_INCREMENT,
+  nome 	VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 -- Insert Data --
 
-Insert into Regiao (Id, Nome) values (1, 'Norte');
-Insert into Regiao (Id, Nome) values (2, 'Nordeste');
-Insert into Regiao (Id, Nome) values (3, 'Sudeste');
-Insert into Regiao (Id, Nome) values (4, 'Sul');
-Insert into Regiao (Id, Nome) values (5, 'Centro-Oeste');
+Insert into regioes (id, nome) values (1, 'Norte');
+Insert into regioes (id, nome) values (2, 'Nordeste');
+Insert into regioes (id, nome) values (3, 'Sudeste');
+Insert into regioes (id, nome) values (4, 'Sul');
+Insert into regioes (id, nome) values (5, 'Centro-Oeste');
